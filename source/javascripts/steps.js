84942f0fca483f311f98fa98abeaa70ce080be98
monique <niqueeta@gmail.com>
2014-12-29 18:57:14 +0000

    Make parent of radio button select radio

--- source/javascripts/steps.js
+++ source/javascripts/steps.js
@@ -1,11 +1,17 @@
-$(document).ready(function() {
-    $(".steps a").click(function(event) {
-        event.preventDefault();
-        $(this).parent().addClass("current");
-        $(this).parent().siblings().removeClass("current");
+// $(document).ready(function() {
+//     $(".steps a").click(function(event) {
+//         event.preventDefault();
+//         $(this).parent().addClass("current");
+//         $(this).parent().siblings().removeClass("current");
 
-        var active = $(this).attr("href");
-        $(".content").not(active).css("display", "none");
-        $(active).fadeIn();
-    });
-});
+//         var active = $(this).attr("href");
+//         $(".content").not(active).css("display", "none");
+//         $(active).fadeIn();
+//     });
+// });
+
+$('.option').click(function (e) {
+	$(this).siblings().removeClass("selected");
+	$(this).addClass("selected");
+    $(this).find('.option-radio').prop('checked', true);
+ });
