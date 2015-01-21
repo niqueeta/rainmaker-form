12be61af28b4ce1f6bec43c3ca52b6e52c57570a
monique <niqueeta@gmail.com>
2015-01-21 13:57:26 +0000

    everything

--- build/javascripts/all.js
+++ build/javascripts/all.js
@@ -9190,18 +9190,109 @@ return jQuery;
 
 }));
 // Tabs show/hide steps
-
 $(document).ready(function() {
-    $(".steps a").click(function(event) {
-        event.preventDefault();
-        $(this).parent().addClass("current");
-        $(this).parent().siblings().removeClass("current");
+  $(".steps a").click(function(event) {
+    event.preventDefault();
+    $(this).parent().addClass("current");
+    $(this).parent().siblings().removeClass("current");
 
-        var active = $(this).attr("href");
-        $(".content").not(active).css("display", "none");
-        $(active).fadeIn();
-    });
+    var active = $(this).attr("href");
+    $(".content").not(active).css("display", "none");
+    $(active).fadeIn();
+  });
 });
+
+// final step instructions
+$('.pay-method input[value=direct-debit]').click(function (e) {
+	$("#last-step").html("<legend>Great! We will send you an email with instructions for how to pay via direct Debit.<label><input class='call-to-action' type='submit' name='join' value='Join the Rainmaker Foundation'></input></label></legend>");
+});
+
+$('.pay-method input[value=direct-debit]').click(function (e) {
+	$("#last-step").css("display", "block");
+	$("#last-step").html("<legend>Great! We will send you an email with instructions for how to pay via Direct Debit.<label><input class='call-to-action' type='submit' name='join' value='Join the Rainmaker Foundation'></input></label></legend>");
+});
+
+$('.pay-method input[value=cheque]').click(function (e) {
+	$("#last-step").css("display", "block");
+	$("#last-step").html("<legend>Great! We will send you an email with instructions for how to pay via cheque.<label><input class='call-to-action' type='submit' name='join' value='Join the Rainmaker Foundation'></input></label></legend>");
+});
+
+$('.pay-method input[value=credit-card]').click(function (e) {
+	$("#last-step").css("display", "block");
+	$("#last-step").html("<legend>Great! We will now redirect you to a PayPal page where you will be able to pay via Credit Card.<label><input class='call-to-action' type='submit' name='join' value='Join the Rainmaker Foundation'></input></label></legend>");
+});
+
+
+// clicking on div selects input
+$('.membership-type .option').click(function (e) {
+	$(this).siblings().removeClass("selected");
+	$(this).addClass("selected");
+  $(this).find('.option-radio').prop('checked', true);
+});
+
+
+// donation amount based on frequency and membership-type
+function donation(frequency, low, mid, high) {
+	$(".donation-amount").html("<label><input type='radio' name='donation-amount' value='" + low + "'>£" + low + " / " + frequency + " </input></label><label><input type='radio' name='donation-amount' value='" + mid + "'>£" + mid + " / " + frequency + " </input></label><label><input type='radio' name='donation-amount' donation-value='" + high + "'>£" + high + " / " + frequency + " </input></label><label><input type='radio' name='donation-amount' value='other-amount'></input>£<input class='other amount' type='text' name='other-amount'></input> / " + frequency + "</label>");
+}
+
+$('.membership-type .option').click(function (e) {
+	$(this).siblings().removeClass("selected");
+	$(this).addClass("selected");
+  $(this).find('.option-radio').prop('checked', true);
+  $('#personal-details').css("display", "block");
+	if( $('.membership-type input[value=individual]').is(':checked')) {
+		$('.family').css("display", "block");
+		donation("year", "1,000", "1,500", "2,000");
+
+	}if( $('.membership-type input[value=family]').is(':checked')) {
+		$('.family').css("display", "none");
+		donation("year", "3,000", "4,500", "6,000");
+
+	}if( $('.membership-type input[value=company]').is(':checked')) {
+		$('.family').css("display", "none");
+		donation("year", "5,000", "7,500", "10,000");
+	} else {}
+});
+
+$('.frequency input[value=annual]').click(function (e) {
+	if( $('.membership-type input[value=individual]').is(':checked')) {
+		$('.family').css("display", "block");
+		donation("year", "1,000", "1,500", "2,000");
+
+	}if( $('.membership-type input[value=family]').is(':checked')) {
+		$('.family').css("display", "none");
+		donation("year", "3,000", "4,500", "6,000");
+
+	}if( $('.membership-type input[value=company]').is(':checked')) {
+		$('.family').css("display", "none");
+		donation("year", "5,000", "7,500", "10,000");
+	} else {}
+});
+
+$('.frequency input[value=monthly]').click(function (e) {
+	if( $('.membership-type input[value=individual]').is(':checked')) {
+		$('.family').css("display", "block");
+		donation("month", "85", "125", "167");
+
+	}if( $('.membership-type input[value=family]').is(':checked')) {
+		$('.family').css("display", "none");
+		donation("month", "250", "375", "500");
+
+	}if( $('.membership-type input[value=company]').is(':checked')) {
+		$('.family').css("display", "none");
+		donation("month", "417", "625", "834");
+	} else {}
+});
+
+
+// selecting other text field automatically selects input
+$('input.other').click(function (e) {
+	$(this).siblings().prop('checked', true);
+});
+
+
+// Selecting whole box on options checks the child radio button 
 
 $('.membership-type .option').click(function (e) {
 	$(this).siblings().removeClass("selected");
@@ -9222,30 +9313,6 @@ $('.membership-type .option').click(function (e) {
 $('input.other').click(function (e) {
 	$(this).siblings().prop('checked', true);
 });
-
-
-// // Selecting whole box on options checks the child radio button 
-
-$('.membership-type .option').click(function (e) {
-	$(this).siblings().removeClass("selected");
-	$(this).addClass("selected");
-  $(this).find('.option-radio').prop('checked', true);
-});
-
-$('.membership-type .option').click(function (e) {
-	if( $('.membership-type input[value=family]').is(':checked')) {
-		$('#personal-details').css("display", "block");
-		$('.family').css("display", "block");
-	} else {
-		$('#personal-details').css("display", "block");
-		$('.family').css("display", "none");
-	}
-});
-
-$('input.other').click(function (e) {
-	$(this).siblings().prop('checked', true);
-});
-
 
 $('.charity-sectors .option').click(function (e) {
 	if( $(this).find('.option-checkbox').is(':checked')) {
